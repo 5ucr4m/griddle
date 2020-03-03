64fe84d3a6ce6daf56f1c9005d68448a0768b841
Marcus Vinicius <5ucr4m@gmail.com>
2020-03-03 13:35:22 -0300

    Change api url

--- service/api.js
+++ service/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 import store from "../store";
 
 const api = axios.create({
-  // baseURL: "http://67.227.214.144:3010/"
-  baseURL: "http://192.168.15.2:3010/"
+  baseURL: "http://67.227.214.144:3010/"
+  // baseURL: "http://192.168.15.2:3010/"
   // baseURL: "http://0f493570.ngrok.io"
 });
 
