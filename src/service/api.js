807b08ee173e652ed08233bcdcaad9a89e075d4c
Marcus Vinicius <5ucr4m@gmail.com>
2020-05-01 14:26:31 -0300

    change api module

--- src/service/api.js
+++ src/service/api.js
@@ -3,8 +3,7 @@ import axios from "axios";
 import store from "../store";
 
 const api = axios.create({
-  // baseURL: "http://67.227.214.144:3010/",
-  baseURL: "http://192.168.15.9:3010/",
+  baseURL: "http://67.227.214.144:3010/",
 });
 
 api.interceptors.request.use(async (config) => {
